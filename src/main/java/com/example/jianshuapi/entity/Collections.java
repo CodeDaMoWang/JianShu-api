6c565ed460cac88663c6fe73ae2591e9545e4812
CodeDaMoWang <x863004120@163.com>
2018-10-22 10:57:18 +0800

    简书后台

---
+++ src/main/java/com/example/jianshuapi/entity/Collections.java
@@ -0,0 +1,19 @@
+package com.example.jianshuapi.entity;
+
+import lombok.Builder;
+import lombok.Data;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+
+
+@Entity
+@Data
+@Builder
+public class Collections {
+    @Id
+    @GeneratedValue
+    private Integer collectionId;
+}
